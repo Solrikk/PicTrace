9d40933a3f6bccc661a84cc79424f708622d382b
Святослав Кёнинг <70236693+Solrikk@users.noreply.github.com>
2024-04-13 13:17:10 +0300

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -4,6 +4,7 @@ import numpy as np
 from database import add_image_to_db, load_db
 from skimage.metrics import structural_similarity as ssim
 import asyncio
+from scipy.spatial import distance as dist
 
 
 async def download_image(url: str) -> np.ndarray:
@@ -20,8 +21,10 @@ async def download_image(url: str) -> np.ndarray:
 async def process_image(image_entry: dict, target_image: np.ndarray) -> tuple:
   try:
     current_image = await download_image(image_entry["url"])
-    target_image_resized = cv2.resize(target_image, (256, 256))
-    current_image_resized = cv2.resize(current_image, (256, 256))
+    target_image_resized = cv2.resize(target_image,
+                                      (512, 512))
+    current_image_resized = cv2.resize(current_image,
+                                       (512, 512))
     target_gray = cv2.cvtColor(target_image_resized, cv2.COLOR_BGR2GRAY)
     current_gray = cv2.cvtColor(current_image_resized, cv2.COLOR_BGR2GRAY)
     ssim_index = ssim(target_gray, current_gray)
@@ -30,13 +33,33 @@ async def process_image(image_entry: dict, target_image: np.ndarray) -> tuple:
         target_gray, None)
     current_keypoints, current_descriptors = orb.detectAndCompute(
         current_gray, None)
+
     if target_descriptors is None or current_descriptors is None:
       return (0, image_entry["url"])
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(target_descriptors, current_descriptors)
+
+    FLANN_INDEX_LSH = 6
+    index_params = dict(algorithm=FLANN_INDEX_LSH,
+                        table_number=6,
+                        key_size=12,
+                        multi_probe_level=1)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.match(target_descriptors, current_descriptors)
     matches = sorted(matches, key=lambda x: x.distance)
-    orb_score = len(matches) / float(len(target_keypoints))
-    final_score = (orb_score + ssim_index) / 2
+    feature_score = len(matches) / float(len(target_keypoints))
+
+    target_hist = cv2.calcHist([target_image_resized], [0, 1, 2], None,
+                               [8, 8, 8], [0, 256, 0, 256, 0, 256])
+    current_hist = cv2.calcHist([current_image_resized], [0, 1, 2], None,
+                                [8, 8, 8], [0, 256, 0, 256, 0, 256])
+    cv2.normalize(target_hist, target_hist)
+    cv2.normalize(current_hist, current_hist)
+    hist_score = cv2.compareHist(target_hist, current_hist, cv2.HISTCMP_CORREL)
+
+    final_score = (feature_score + ssim_index +
+                   hist_score) / 3
+
     return (final_score, image_entry["url"])
   except Exception as e:
     print(f"Failed to process image {image_entry['url']} due to {e}")
