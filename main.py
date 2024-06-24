4fb852eae8da0f1cf2675cdda26e6b7a9c22aca1
Sviatoslav König <reveni324@gmail.com>
2024-06-24 14:14:18 +0000

    # add: new configuration

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI, File, UploadFile, Form
-from fastapi.responses import HTMLResponse
-import aiohttp
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse, FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import hashlib
 import os
-from image_utils import find_similar_images, download_image
+from image_utils import find_similar_images
 from database import init_db, add_image_to_db
 
 app = FastAPI()
-
+х
 init_db()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -18,7 +20,7 @@ async def upload_form():
   return HTMLResponse(content=html_content)
 
 
-@app.post("/upload/")
+@app.post("/upload/", response_class=JSONResponse)
 async def create_upload_file(file: UploadFile = File(...)):
   contents = await file.read()
   image_hash = hashlib.sha256(contents).hexdigest()
@@ -28,30 +30,17 @@ async def create_upload_file(file: UploadFile = File(...)):
     f.write(contents)
   add_image_to_db(file_path, image_hash)
   similar_images = await find_similar_images(file_path)
-  return {
+
+  response_data = {
       "message": "File uploaded successfully",
-      "filename": file.filename,
+      "filename": image_hash,
       "similar_images": similar_images
   }
 
+  return JSONResponse(content=response_data)
 
-@app.post("/upload_from_url/")
-async def upload_from_url(image_url: str = Form(...)):
-  async with aiohttp.ClientSession() as session:
-    async with session.get(image_url) as response:
-      if response.status == 200:
-        contents = await response.read()
-        image_hash = hashlib.sha256(contents).hexdigest()
-        file_path = f'uploads/{image_hash}.jpg'
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, 'wb') as f:
-          f.write(contents)
-        add_image_to_db(file_path, image_hash)
-        similar_images = await find_similar_images(file_path)
-        return {
-            "message": "File uploaded successfully from URL",
-            "filename": os.path.basename(file_path),
-            "similar_images": similar_images
-        }
-      else:
-        return {"message": "Failed to download image from URL"}
+
+@app.get("/uploads/{image_hash}.jpg", response_class=FileResponse)
+async def serve_uploaded_image(image_hash: str):
+  file_path = f'uploads/{image_hash}.jpg'
+  return FileResponse(file_path)
