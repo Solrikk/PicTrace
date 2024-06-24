018fd9b9a1e67b01fe8bd93608592b891f055a8b
Sviatoslav König <reveni324@gmail.com>
2024-06-24 14:12:15 +0000

    # add: new configuration

--- image_utils.py
+++ image_utils.py
@@ -8,6 +8,7 @@ from tensorflow.keras.preprocessing import image
 from database import load_db
 import asyncio
 from sklearn.metrics.pairwise import cosine_similarity
+import os
 
 base_model = ResNet50(weights='imagenet', include_top=False, pooling='avg')
 model = Model(inputs=base_model.input, outputs=base_model.output)
@@ -20,7 +21,7 @@ async def download_image(session, url):
       image_array = np.frombuffer(image_data, np.uint8)
       img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
       return img
-  raise Exception('Image download failed')
+    raise Exception('Image download failed')
 
 
 def extract_features(img):
@@ -32,15 +33,16 @@ def extract_features(img):
   return features
 
 
-async def compare_images(session, image_url, target_features):
+async def compare_images(session, image_data, target_features):
   try:
-    current_image = await download_image(session, image_url)
+    current_image = await download_image(session, image_data["url"])
     current_features = extract_features(current_image)
     similarity = cosine_similarity(current_features, target_features)
-    return (similarity[0][0], image_url)
+    if similarity[0][0] > 0.5:
+      return (similarity[0][0], image_data["url"])
   except Exception as e:
-    print(f"Failed to compare image {image_url} due to {e}")
-    return (0, image_url)
+    print(f"Failed to compare image {image_data['url']} due to {e}")
+  return (0, "")
 
 
 async def find_similar_images(file_path):
@@ -49,16 +51,17 @@ async def find_similar_images(file_path):
   target_features = extract_features(target_image)
   async with aiohttp.ClientSession() as session:
     tasks = [
-        compare_images(session, entry["url"], target_features)
-        for entry in db_data if "url" in entry
+        compare_images(session, entry, target_features) for entry in db_data
+        if "url" in entry
     ]
     results = await asyncio.gather(*tasks)
+
   valid_results = filter(lambda x: x[0] > 0, results)
   sorted_results = sorted(valid_results, key=lambda x: x[0], reverse=True)[:5]
-  seen_urls = set()
-  top_similar_images = []
+
+  similar_images = []
   for result in sorted_results:
-    if result[1] not in seen_urls:
-      top_similar_images.append(result[1])
-      seen_urls.add(result[1])
-  return top_similar_images
+    if result[1]:
+      similar_images.append(result[1])
+
+  return similar_images
