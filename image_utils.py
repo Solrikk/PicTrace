2325e30c9d40671b9fa0f02c8a677b264b3f6ca5
Sviatoslav König <reveni324@gmail.com>
2024-07-18 11:44:26 +0300

    # feat: add category detection for People images
    - Added is_people_image function to classify images containing people.
    - Extended find_similar_images function to handle and search for People category.
    - Updated folder classification to include "People" folder.

--- image_utils.py
+++ image_utils.py
@@ -1,12 +1,14 @@
-import aiohttp
-import cv2
-import numpy as np
 import boto3
 from botocore.client import Config
-from concurrent.futures import ThreadPoolExecutor
+from botocore.exceptions import ClientError
+import cv2
+import numpy as np
 import asyncio
-from classification_utils import is_architecture_image, is_aviation_image, is_background_image, extract_features_batch
-from sklearn.metrics.pairwise import cosine_similarity  # Новый импорт
+from concurrent.futures import ThreadPoolExecutor
+from classification_utils import (is_architecture_image, is_aviation_image,
+                                  is_background_image, is_animal_image,
+                                  is_people_image, extract_features_batch)
+from sklearn.metrics.pairwise import cosine_similarity
 
 S3_BUCKET_NAME = 'YOUR_S3_BUCKET_NAME'
 S3_REGION = 'YOUR_S3_REGION'
@@ -21,37 +23,6 @@ s3_client = boto3.client(
     aws_secret_access_key=S3_SECRET_ACCESS_KEY,
     config=Config(region_name=S3_REGION, signature_version='s3v4'),
 )
-
-
-async def download_image_from_s3(file_name):
-  try:
-    response = s3_client.get_object(Bucket=S3_BUCKET_NAME, Key=file_name)
-    image_data = response['Body'].read()
-    image_array = np.frombuffer(image_data, np.uint8)
-    img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    if img is None:
-      raise ValueError("Failed to decode image")
-    return img
-  except Exception as e:
-    print(f"Failed to download image {file_name} from S3 due to {e}")
-    return None
-
-
-async def compare_images(file_name, target_features, loop):
-  try:
-    current_image = await download_image_from_s3(file_name)
-    if current_image is None:
-      return (0, "")
-    current_features = await loop.run_in_executor(
-        None, lambda: extract_features_batch([current_image]))
-    if current_features.size == 0:
-      return (0, "")
-    similarity = cosine_similarity(current_features, target_features)
-    if similarity[0][0] > 0.5:
-      return (similarity[0][0], file_name)
-  except Exception as e:
-    print(f"Failed to compare image {file_name} due to {e}")
-  return (0, "")
 
 
 async def list_s3_images(folder=''):
@@ -76,9 +47,39 @@ async def list_s3_images(folder=''):
       else:
         break
     return images
-  except Exception as e:
+  except ClientError as e:
     print(f"Failed to list images in S3 bucket: {e}")
-    raise Exception('Listing images in S3 failed')
+    raise Exception('Listing images in S3 failed') from e
+
+
+def download_image_from_s3(file_name):
+  try:
+    response = s3_client.get_object(Bucket=S3_BUCKET_NAME, Key=file_name)
+    image_data = response['Body'].read()
+    image_array = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    return image
+  except ClientError as e:
+    print(f"Failed to download image {file_name} from S3: {e}")
+    return None
+
+
+async def compare_images(file_name, target_features, loop):
+  try:
+    current_image = download_image_from_s3(file_name)
+    if current_image is None:
+      return (0, "")
+    with ThreadPoolExecutor() as pool:
+      current_features = await loop.run_in_executor(
+          pool, lambda: extract_features_batch([current_image]))
+    if current_features.size == 0:
+      return (0, "")
+    similarity = cosine_similarity(current_features, target_features)
+    if similarity[0][0] > 0.5:
+      return (similarity[0][0], file_name)
+  except Exception as e:
+    print(f"Failed to compare image {file_name} due to {e}")
+  return (0, "")
 
 
 async def find_similar_images(file_path):
@@ -86,6 +87,8 @@ async def find_similar_images(file_path):
   is_architecture = await is_architecture_image(file_path)
   is_aviation = await is_aviation_image(file_path)
   is_background = await is_background_image(file_path)
+  is_animal = await is_animal_image(file_path)
+  is_people = await is_people_image(file_path)
 
   if is_architecture:
     folder = "Architecture/"
@@ -93,6 +96,10 @@ async def find_similar_images(file_path):
     folder = "Aviation/"
   elif is_background:
     folder = "Backgrounds/"
+  elif is_animal:
+    folder = "Animals/"
+  elif is_people:
+    folder = "People/"
   else:
     folder = ''
 
@@ -103,7 +110,6 @@ async def find_similar_images(file_path):
   target_features = extract_features_batch([target_image])
 
   file_names = await list_s3_images(folder=folder)
-
   tasks = [
       compare_images(file_name, target_features, loop)
       for file_name in file_names
