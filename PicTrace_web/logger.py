e2076b3997784cde30422c37d54c6b404d57e0fc
Sviatoslav König <reveni324@gmail.com>
2024-12-30 15:57:41 +0300

    Add files via upload

---
+++ PicTrace_web/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.StreamHandler()
+        ]
+    )
